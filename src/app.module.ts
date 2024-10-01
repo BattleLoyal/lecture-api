ff73cd31582b0cedd2b51ecc61fb3c5af6082bab
BattleLoyal <dbslxl12345@gmail.com>
2024-10-02 01:42:49 +0900

    Mysql DB 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LectureModule } from './lecture/lecture.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LectureModule],
+  imports: [
+    LectureModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
